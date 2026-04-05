0036cb8b331fdd805c08fc3fd4dc2ab19c1d64a6
miil <tgy_scut2021@outlook.com>
2026-04-05 22:42:11 +0800

    Make batch exe script.

---
+++ runall.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+make run SAMPLE=samples/sample0.txt > log0.log
+make run SAMPLE=samples/sample1.txt > log1.log
+make run SAMPLE=samples/sample2.txt > log2.log
+make run SAMPLE=samples/sample3.txt > log3.log
+make run SAMPLE=samples/sample4.txt > log4.log
